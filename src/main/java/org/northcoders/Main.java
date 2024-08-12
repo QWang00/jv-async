b7ed31b3fbd7223e6038ea2256c438c64021af26
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-12 11:54:29 +0100

    feat: complete step 3

--- src/main/java/org/northcoders/Main.java
+++ src/main/java/org/northcoders/Main.java
@@ -7,37 +7,65 @@ import java.util.concurrent.CompletableFuture;
 public class Main {
 
         public static void main(String[] args) {
+//            System.out.println("Future for step 1: " );
+//            CompletableFuture<Void> step1Future = CompletableFuture.runAsync(()->{
+//                try {
+//                    Thread.sleep(2000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                System.out.println("Hello World!");
+//            });
+//            step1Future.join();
+//
+//            System.out.println("Futures for step 2: ");
+//            CompletableFuture<Void> future1 = CompletableFuture.runAsync(()->{
+//                try {
+//                    Thread.sleep(3000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                System.out.println("Hello...");
+//            });
+//
+//            CompletableFuture<Void> future2 = CompletableFuture.runAsync(()->{
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                System.out.println("...World!");
+//            });
+//
+//            CompletableFuture<Void> joinFutures = CompletableFuture.allOf(future1, future2);
+//            joinFutures.join();
 
-            CompletableFuture<Void> future = CompletableFuture.runAsync(()->{
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-                System.out.println("Hello World!");
+            System.out.println("Futures for step 3, in 5 seconds it will show: ");
+            CompletableFuture<String> future3 = CompletableFuture.supplyAsync(()->{
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return "Hello...";
             });
-            future.join();
 
-            CompletableFuture<Void> future1 = CompletableFuture.runAsync(()->{
-        try {
-        Thread.sleep(3000);
-    } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-    }
-        System.out.println("Hello...");
-    });
-
-            CompletableFuture<Void> future2 = CompletableFuture.runAsync(()->{
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-                System.out.println("...World!");
+            CompletableFuture<String> future4 = CompletableFuture.supplyAsync(()->{
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return "...World!";
             });
 
-            CompletableFuture<Void> joinFutures = CompletableFuture.allOf(future1, future2);
-            joinFutures.join();
+            CompletableFuture<String> combined = future3.thenCombine(future4, (f3, f4)-> f3 + " " + f4);
+            String result = combined.join();
+            System.out.println(result);
+
+
+
+
         }
 }
 
