32d46d753e1228fc86b2c5b303a71678241177d1
Amanda Gabriele Alves Ferreira <amanda@amandas-mbp.home>
2024-08-12 11:39:04 +0100

    feat: complete task 2

--- src/main/java/org/northcoders/Main.java
+++ src/main/java/org/northcoders/Main.java
@@ -7,14 +7,37 @@ import java.util.concurrent.CompletableFuture;
 public class Main {
 
         public static void main(String[] args) {
+
+            CompletableFuture<Void> future = CompletableFuture.runAsync(()->{
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            CompletableFuture<Void> future = CompletableFuture.runAsync(()->{
                 System.out.println("Hello World!");
             });
             future.join();
-        }
+
+            CompletableFuture<Void> future1 = CompletableFuture.runAsync(()->{
+        try {
+        Thread.sleep(3000);
+    } catch (InterruptedException e) {
+        throw new RuntimeException(e);
     }
+        System.out.println("Hello...");
+    });
+
+            CompletableFuture<Void> future2 = CompletableFuture.runAsync(()->{
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+                System.out.println("...World!");
+            });
+
+            CompletableFuture<Void> joinFutures = CompletableFuture.allOf(future1, future2);
+            joinFutures.join();
+        }
+}
+
