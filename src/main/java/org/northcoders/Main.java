f84b422a0948d6b73df018399e1e341ae12ecf85
Amanda Gabriele Alves Ferreira <amanda@amandas-mbp.home>
2024-08-12 12:17:32 +0100

    feat: complete task 4

--- src/main/java/org/northcoders/Main.java
+++ src/main/java/org/northcoders/Main.java
@@ -1,6 +1,7 @@
 package org.northcoders;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -40,32 +41,66 @@ public class Main {
 //            CompletableFuture<Void> joinFutures = CompletableFuture.allOf(future1, future2);
 //            joinFutures.join();
 
-            System.out.println("Futures for step 3, in 5 seconds it will show: ");
-            CompletableFuture<String> future3 = CompletableFuture.supplyAsync(()->{
+//            System.out.println("Futures for step 3, in 5 seconds it will show: ");
+//            CompletableFuture<String> future3 = CompletableFuture.supplyAsync(()->{
+//                try {
+//                    Thread.sleep(3000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                return "Hello...";
+//            });
+//
+//            CompletableFuture<String> future4 = CompletableFuture.supplyAsync(()->{
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                return "...World!";
+//            });
+//
+//            CompletableFuture<String> combined = future3.thenCombine(future4, (f3, f4)-> f3 + " " + f4);
+//            String result = combined.join();
+//            System.out.println(result);
+
+            System.out.println("Futures for step 4, within 10 seconds it will show: ");
+            int min = 1000;
+            int max = 10000;
+            int random1 = min + (int) (Math.random() * ((max - min)) + 1);
+            int random2 = min + (int) (Math.random() * ((max - min)) + 1);
+
+                        CompletableFuture<String> future5 = CompletableFuture.supplyAsync(()->{
                 try {
-                    Thread.sleep(3000);
+                    System.out.println(random1);
+                    Thread.sleep(random1);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 return "Hello...";
             });
 
-            CompletableFuture<String> future4 = CompletableFuture.supplyAsync(()->{
+            CompletableFuture<String> future6 = CompletableFuture.supplyAsync(()->{
                 try {
-                    Thread.sleep(5000);
+                    System.out.println(random2);
+                    Thread.sleep(random2);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 return "...World!";
             });
 
-            CompletableFuture<String> combined = future3.thenCombine(future4, (f3, f4)-> f3 + " " + f4);
-            String result = combined.join();
-            System.out.println(result);
 
-
-
+                CompletableFuture<String> combined = future5.thenCombine(future6, (f5, f6)-> f5 + " " + f6);
+            try {
+                String result = combined.join();
+                System.out.println(result);
+            } catch (Exception e) {
+                System.out.println("Error: exceeded 10 seconds.");
+                throw new RuntimeException(e);
+                }
+            }
 
         }
-}
+
 
